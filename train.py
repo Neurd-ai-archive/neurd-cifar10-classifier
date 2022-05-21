e73c33495ee748e4fe68ee5783a5b3541d6a97e3
Lugges991 <29121982+Lugges991@users.noreply.github.com>
2022-05-22 00:38:41 +0200

    initial commit

---
+++ train.py
@@ -0,0 +1,53 @@
+import torch
+import torchvision
+import torch.optim as optim
+import torch.nn as nn
+import torchvision.transforms as transforms
+from models import SimpleCNN
+
+
+def train():
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    batch_size = 4
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    model = SimpleCNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    n_epochs = 10
+
+    for epoch in range(n_epochs):
+        print("**************")
+        print("Starting epoch {epoch}...")
+        running_loss = 0.00
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            optimizer.zero_grad()
+
+            out = model(inputs)
+            loss = criterion(out, labels)
+            loss.backward()
+            optimizer.step
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+    print("Finished Training")
+
+    torch.save(model.state_dict(), "./data/saved_model.pth")
+
+
+if __name__ == "__main__":
+    
+    train()
